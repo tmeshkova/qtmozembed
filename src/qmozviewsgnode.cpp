0490b6fb864318d4e8578540cbbe2d22c90bc918
Tatiana Meshkova <tanya.meshkova@gmail.com>
2013-07-01 21:54:05 -0700

    Removed harmfull clipping code.

--- src/qmozviewsgnode.cpp
+++ src/qmozviewsgnode.cpp
@@ -31,12 +31,6 @@ public:
         QMatrix affine = matrix() ? (*matrix()).toAffine() : QMatrix();
         gfxMatrix matr(affine.m11(), affine.m12(), affine.m21(), affine.m22(), affine.dx(), affine.dy());
         mPrivate->mView->SetGLViewTransform(matr);
-        if (state.scissorEnabled) {
-            mPrivate->mView->SetViewClipping(state.scissorRect.x(),
-                                               state.scissorRect.x(),
-                                               state.scissorRect.width(),
-                                               state.scissorRect.height());
-        }
         mPrivate->mView->RenderGL();
     }
 
@@ -54,46 +48,6 @@ public:
     QGraphicsMozViewPrivate* getPrivate() { return mPrivate; }
 
 private:
-    QRectF clipRect() const
-    {
-        // Start with an invalid rect.
-        QRectF resultRect(0, 0, -1, -1);
-
-        for (const QSGClipNode* clip = clipList(); clip; clip = clip->clipList()) {
-            QMatrix4x4 clipMatrix = *clip->matrix();
-
-            QRectF currentClip;
-
-            if (clip->isRectangular())
-                currentClip = clipMatrix.mapRect(clip->clipRect());
-            else {
-                const QSGGeometry* geometry = clip->geometry();
-                // Assume here that clipNode has only coordinate data.
-                const QSGGeometry::Point2D* geometryPoints = geometry->vertexDataAsPoint2D();
-
-                // Clip region should be at least triangle to make valid clip.
-                if (geometry->vertexCount() < 3)
-                    continue;
-
-                QPolygonF polygon;
-
-                for (int i = 0; i < geometry->vertexCount(); i++)
-                    polygon.append(clipMatrix.map(QPointF(geometryPoints[i].x, geometryPoints[i].y)));
-                currentClip = polygon.boundingRect();
-            }
-
-            if (currentClip.isEmpty())
-                continue;
-
-            if (resultRect.isValid())
-                resultRect &= currentClip;
-            else
-                resultRect = currentClip;
-        }
-
-        return resultRect;
-    }
-
     QGraphicsMozViewPrivate* mPrivate;
 };
 
