e9e9f8921454d1403a04fa9509880e23b02134af
Dmitry Rozhkov <dmitry.rozhkov@jollamobile.com>
2013-12-16 18:36:35 +0200

    [qtmozembed] Don't cancel touch upon MOZVIEW_FLICK_THRESHOLD

--- src/qgraphicsmozview_p.cpp
+++ src/qgraphicsmozview_p.cpp
@@ -644,8 +644,7 @@ void QGraphicsMozViewPrivate::touchEvent(QTouchEvent* event)
     qint64 timeStamp = current_timestamp(event);
     MultiTouchInput meventStart(MultiTouchInput::MULTITOUCH_START, timeStamp, 0);
     MultiTouchInput meventMove(MultiTouchInput::MULTITOUCH_MOVE, timeStamp, 0);
-    MultiTouchInput meventEnd(mCanFlick ? MultiTouchInput::MULTITOUCH_END :
-                              MultiTouchInput::MULTITOUCH_CANCEL, timeStamp, 0);
+    MultiTouchInput meventEnd(MultiTouchInput::MULTITOUCH_END, timeStamp, 0);
 
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
     // Add active touch point to cancelled touch sequence.
